25562a40ee6e08360ef5b82ea564403259ff9e27
Ferdinand <fercalo96@yahoo.de>
2019-12-03 18:27:33 +0100

    Rules sync for GUI and other.

--- src/main/java/net/fexcraft/mod/states/guis/RulesUI.java
+++ src/main/java/net/fexcraft/mod/states/guis/RulesUI.java
@@ -1,11 +1,6 @@
 package net.fexcraft.mod.states.guis;
 
 import net.fexcraft.lib.mc.gui.GenericGui;
-import net.fexcraft.lib.mc.utils.Print;
-import net.fexcraft.mod.states.data.Chunk;
-import net.fexcraft.mod.states.data.Rule;
-import net.fexcraft.mod.states.data.root.RuleHolder;
-import net.fexcraft.mod.states.util.StateUtil;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
@@ -16,44 +11,53 @@ import net.minecraftforge.fml.relauncher.Side;
 public class RulesUI extends GenericGui<RulesUIC> {
 	
 	private static final ResourceLocation texture = new ResourceLocation("states:textures/gui/rules.png");
+	private BasicButton[] rev = new BasicButton[16], set = new BasicButton[16];
 	private TextField[] tfields = new TextField[16];
-	private Rule[] rules = new Rule[16];
 	private BasicText title, status;
-	private RuleHolder holder;
-	private int page, layer;
 
 	public RulesUI(EntityPlayer player, World world, int x, int y, int z){
 		super(texture, new RulesUIC(player, world, x, y, z), player);
 		this.defbackground = true; this.deftexrect = true; container.gui = this;
-		this.xSize = 248; this.ySize = 240; page = y < 0 ? 0 : y; layer = x;
-		Chunk chunk = StateUtil.getChunk(player.getPosition());
-		switch(layer){
-			//case 0: holder = chunk; break;
-			case 1: holder = chunk.getDistrict(); break;
-			case 2: holder = chunk.getMunicipality(); break;
-			//case 3: holder = chunk.getState(); break;
-			default: {
-				Print.log("Invalid Layer for Rules GUI"); player.closeScreen(); break;
-			}
-		}
-		Rule[] arr = holder.getRules().values().toArray(new Rule[0]); int j = 0;
-		for(int i = 0; i < 16; i++){
-			if((j = i + (page * 16)) >= holder.getRules().size()) break;
-			rules[i] = arr[j]; continue;
-		}
+		this.xSize = 248; this.ySize = 240;
 	}
 
 	@Override
 	protected void init(){
+		NBTTagCompound compound = new NBTTagCompound(); compound.setString("cargo", "init");
+		container.send(Side.SERVER, compound);
+		texts.put("title", title = new BasicText(guiLeft + 10, guiTop + 8, 216, MapColor.SNOW.colorValue, "waiting for server response..."));
+		texts.put("status", status = new BasicText(guiLeft + 10, guiTop + 225, 216, MapColor.SNOW.colorValue, "waiting for server response..."));
+	}
+	
+	protected void initFromContainer(){
 		for(int i = 0; i < 16; i++){
-			buttons.put("set" + i, new BasicButton("set" + i, guiLeft + 215, guiTop + 25 + (i * 12), 215, 25, 10, 10, rules[i] != null && rules[i].get() != null));
-			buttons.put("rev" + i, new BasicButton("rev" + i, guiLeft + 204, guiTop + 25 + (i * 12), 204, 25, 10, 10, true));
-			fields.put("entry" + i, tfields[i] = new TextField(i, fontRenderer, guiLeft + 150, guiTop + 25 + (i * 12), 53, 10, false));
-			texts.put("rule" + i, new BasicText(guiLeft + 12, guiTop + 26 + (i * 12), 134, MapColor.SNOW.colorValue, rules[i] == null ? "" : rules[i].id));
-			tfields[i].setText("sel. mode");
+			buttons.put("set" + i, set[i] = new BasicButton("set" + i, guiLeft + 215, guiTop + 25 + (i * 12), 215, 25, 10, 10, container.rules[i] != null && container.rules[i].get() != null));
+			buttons.put("rev" + i, rev[i] = new BasicButton("rev" + i, guiLeft + 204, guiTop + 25 + (i * 12), 204, 25, 10, 10, true));
+			if(i != 15) fields.put("entry" + i, tfields[i] = new TextField(i, fontRenderer, guiLeft + 150, guiTop + 25 + (i * 12), 53, 10, false));
+			else{
+				fields.put("entry" + i, tfields[i] = new TextField(i, fontRenderer, guiLeft + 150, guiTop + 25 + (i * 12), 53, 10, false){
+					@Override
+					public void drawTextBox(){
+						super.drawTextBox(); if(set[0] == null) return;
+						for(int i = 0; i < 16; i++){
+							if(set[i].hovered) drawHoveringText("Set Value of this Rule.", set[i].x, set[i].y);
+							if(rev[i].hovered) drawHoveringText("Revise this Rule.", rev[i].x, rev[i].y);
+						};
+					}
+				});
+			}
+			texts.put("rule" + i, new BasicText(guiLeft + 12, guiTop + 26 + (i * 12), 134, MapColor.SNOW.colorValue, container.rules[i] == null ? "" : container.rules[i].id));
+			tfields[i].setText(container.rules[i] == null ? "" : "sel. mode"); tfields[i].setEnabled(container.rules[i] != null);
 		}
 		buttons.put("next", new BasicButton("next", guiLeft + 230, guiTop + 63, 230, 63, 14, 14, true));
-		buttons.put("prev", new BasicButton("prev", guiLeft + 230, guiTop + 49, 230, 49, 14, 14, page > 0));
+		buttons.put("prev", new BasicButton("prev", guiLeft + 230, guiTop + 49, 230, 49, 14, 14, container.page > 0));
+		switch(container.layer){
+			case 0: title.string = "Chunk Rules"; break;
+			case 1: title.string = "Dis.: " + container.ruleset; break;
+			case 2: title.string = "Mun.: " + container.ruleset; break;
+			case 4: title.string = "State: " + container.ruleset; break;
+			default: title.string = "INVALID LAYER"; break;
+		} status.string = "";
 	}
 
 	@Override
@@ -70,8 +74,14 @@ public class RulesUI extends GenericGui<RulesUIC> {
 	protected boolean buttonClicked(int mouseX, int mouseY, int mouseButton, String key, BasicButton button){
 		if(button.name.equals("prev") || button.name.equals("next")){
 			NBTTagCompound compound = new NBTTagCompound(); compound.setString("cargo", "open");
-			compound.setIntArray("arr", new int[]{ layer, page + (button.name.equals("next") ? 1 : -1), 0 });
+			compound.setIntArray("arr", new int[]{ container.layer, container.page + (button.name.equals("next") ? 1 : -1), 0 });
 			this.container.send(Side.SERVER, compound); return true;
+		}
+		if(button.name.startsWith("set") && button.enabled){
+			//TODO check perm
+		}
+		if(button.name.startsWith("rev")){
+			//TODO check perm
 		}
 		return false;
 	}
--- src/main/java/net/fexcraft/mod/states/guis/RulesUIC.java
+++ src/main/java/net/fexcraft/mod/states/guis/RulesUIC.java
@@ -2,7 +2,14 @@ package net.fexcraft.mod.states.guis;
 
 import net.fexcraft.lib.mc.gui.GenericContainer;
 import net.fexcraft.lib.mc.gui.GenericGui;
+import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.states.States;
+import net.fexcraft.mod.states.data.Chunk;
+import net.fexcraft.mod.states.data.Rule;
+import net.fexcraft.mod.states.data.root.Initiator;
+import net.fexcraft.mod.states.data.root.RuleHolder;
+import net.fexcraft.mod.states.data.root.Ruleable;
+import net.fexcraft.mod.states.util.StateUtil;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
@@ -11,18 +18,61 @@ import net.minecraftforge.fml.relauncher.Side;
 public class RulesUIC extends GenericContainer {
 	
 	protected GenericGui<RulesUIC> gui;
+	protected Rule[] rules = new Rule[16];
+	protected RuleHolder holder;
+	protected int page, layer;
+	protected String ruleset;
 
 	public RulesUIC(EntityPlayer player, World world, int x, int y, int z){
-		super(player);
-		//
+		super(player); page = y < 0 ? 0 : y; layer = x;
+		if(world.isRemote) return;
+		Chunk chunk = StateUtil.getChunk(player.getPosition());
+		switch(layer){
+			//case 0: holder = chunk; break;
+			case 1: holder = chunk.getDistrict(); break;
+			case 2: holder = chunk.getMunicipality(); break;
+			//case 3: holder = chunk.getState(); break;
+			default: {
+				Print.log("Invalid Layer for Rules GUI"); player.closeScreen(); break;
+			}
+		}
+		Rule[] arr = holder.getRules().values().toArray(new Rule[0]); int j = 0;
+		for(int i = 0; i < 16; i++){
+			if((j = i + (page * 16)) >= holder.getRules().size()) break;
+			rules[i] = arr[j]; continue;
+		}
 	}
 
 	@Override
 	protected void packet(Side side, NBTTagCompound packet, EntityPlayer player){
 		if(!packet.hasKey("cargo")) return;
-		if(packet.getString("cargo").equals("open")){
-			int[] arr = packet.getIntArray("arr");
-			player.openGui(States.MODID, 9, player.world, arr[0], arr[1], arr[2]);
+		if(side.isServer()){
+			if(packet.getString("cargo").equals("open")){
+				int[] arr = packet.getIntArray("arr");
+				player.openGui(States.MODID, 9, player.world, arr[0], arr[1], arr[2]);
+			}
+			if(packet.getString("cargo").equals("init")){
+				NBTTagCompound compound = new NBTTagCompound(); compound.setString("cargo", "init");
+				for(int i = 0; i < 16; i++){
+					if(rules[i] == null) break;
+					compound.setString("rule" + i, rules[i].id + "," + rules[i].save());
+				}
+				if(holder instanceof Ruleable) compound.setString("ruleset", ((Ruleable)holder).getRulesetTitle());
+				send(Side.CLIENT, compound);
+			}
+		}
+		else{
+			if(packet.getString("cargo").equals("init")){
+				rules = new Rule[16];
+				for(int i = 0; i < 16; i++){
+					if(!packet.hasKey("rule" + i)) break;
+					String[] arr = packet.getString("rule" + i).split(",");
+					Boolean bool = arr.length > 3 ? Boolean.parseBoolean(arr[3]) : null;
+					rules[i] = new Rule(arr[0], bool, true, Initiator.valueOf(arr[1]), Initiator.valueOf(arr[2]));
+				}
+				if(packet.hasKey("ruleset")) ruleset = packet.getString("ruleset");
+				((RulesUI)gui).initFromContainer();
+			}
 		}
 	}
 

7bf6f7d6142d71e0ff2365a4e8c31a1d509be3dc
Ferdinand <fercalo96@yahoo.de>
2021-06-24 22:17:25 +0200

    Update MunCt.java

--- src/main/java/net/fexcraft/mod/states/data/root/MunCt.java
+++ src/main/java/net/fexcraft/mod/states/data/root/MunCt.java
@@ -1,8 +1,12 @@
 package net.fexcraft.mod.states.data.root;
 
+import com.google.gson.JsonObject;
+
+import net.fexcraft.lib.common.json.JsonUtil;
 import net.fexcraft.mod.states.data.County;
 import net.fexcraft.mod.states.data.District;
 import net.fexcraft.mod.states.data.Municipality;
+import net.fexcraft.mod.states.util.StateUtil;
 
 /**
  * "Municipality-County"
@@ -10,7 +14,7 @@ import net.fexcraft.mod.states.data.Municipality;
  * @author Ferdinand Calo' (FEX___96)
  *
  */
-public class MunCt {
+public class MunCt implements Loadable {
 	
 	public District district;
 	public Municipality municipality;
@@ -54,6 +58,20 @@ public class MunCt {
 		if(other instanceof MunCt == false) return false;
 		MunCt munct = (MunCt)other;
 		return mun == munct.mun && (mun ? municipality.getId() == munct.municipality.getId() : county.getId() == munct.county.getId());
+	}
+
+	public void load(JsonObject obj){
+		municipality = obj.has("municipality") ? StateUtil.getMunicipality(JsonUtil.getIfExists(obj, "municipality", -1).intValue()) : null;
+		county = obj.has("county") ?  StateUtil.getCounty(JsonUtil.getIfExists(obj, "county", -1).intValue()) : null;
+		if(county == null && municipality == null) municipality = StateUtil.getMunicipality(-1);
+		if(county != null) municipality = null;
+		if(municipality != null) mun = true;
+	}
+
+	@Override
+	public void save(JsonObject obj){
+		if(mun) obj.addProperty("municipality", municipality.getId());
+		else obj.addProperty("county", county.getId());
 	}
 
 }
